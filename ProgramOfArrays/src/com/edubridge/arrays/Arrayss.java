6fb9ec134237591348996f83bdbed42b4bd01950
DELL <DELL@DESKTOP-93MJSQO>
2024-01-29 12:32:40 +0530

    commit

---
+++ ProgramOfArrays/src/com/edubridge/arrays/Arrayss.java
@@ -0,0 +1,23 @@
+package com.edubridge.arrays;
+
+import java.util.*;
+
+public class Arrayss {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int size = sc.nextInt();
+		int numbers[]=new int[size];
+		
+		for (int i=0; i<size; i++)
+		{
+			numbers [i]= sc.nextInt();
+		}
+		
+		for (int i=0; i< size; i++)
+			System.out.println(numbers[i]);
+		
+
+	}
+
+}
